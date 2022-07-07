4413bb7e411fa9a556f8162e1e8b31db5ca1b06f
Heleo2705 <heleonidasspartan@gmail.com>
2022-07-07 22:09:44 +0530

    Validation logic added

--- lib/presentation/login_screen.dart
+++ lib/presentation/login_screen.dart
@@ -16,6 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   String email = "";
   String password = "";
+  bool _showPassword = false;
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -24,72 +25,104 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Form(
           key: _formKey,
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              BlocBuilder<LoginBloc, LoginState>(
-                builder: (context, state) {
-                  return Card(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                            label: Text("Username"),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BlocBuilder<LoginBloc, LoginState>(
+                  builder: (context, state) {
+                    return Card(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            validator: (v) {
+                              final regex =
+                                  RegExp(r'^\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b');
+                              if (v != null) {
+                                if (regex.hasMatch(v) != true) {
+                                  return "Please enter a valid email";
+                                }
+                              }
+                            },
+                            decoration: InputDecoration(
+                              label: Text("Username"),
+                            ),
+                            onChanged: (v) {
+                              email = v;
+                              context.read<LoginBloc>().add(
+                                    TextInputChanged(
+                                      formValid:
+                                          _formKey.currentState?.validate() ??
+                                              false,
+                                    ),
+                                  );
+                            },
                           ),
-                          onChanged: (v) {
-                            email = v;
-                            context.read<LoginBloc>().add(
-                                  TextInputChanged(
-                                    formValid:
-                                        _formKey.currentState?.validate() ??
-                                            false,
-                                  ),
-                                );
-                          },
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            label: Text("Password"),
+                          TextFormField(
+                            obscureText: !_showPassword,
+                            enableSuggestions: false,
+                            autocorrect: false,
+                            decoration: InputDecoration(
+                              label: Text("Password"),
+                              suffixIcon: InkWell(
+                                child: Icon(Icons.visibility),
+                                onTap: () {
+                                  setState(() {
+                                    _showPassword = !_showPassword;
+                                  });
+                                },
+                              ),
+                              suffixIconColor:
+                                  (!_showPassword) ? Colors.grey : Colors.blue,
+                            ),
+                            validator: (v) {
+                              if (v != null) {
+                                if (v.trim().length < 6) {
+                                  return "Password should be at least 6 digits long";
+                                }
+                              }
+                            },
+                            onChanged: (v) {
+                              password = v;
+                              context.read<LoginBloc>().add(
+                                    TextInputChanged(
+                                      formValid:
+                                          _formKey.currentState?.validate() ??
+                                              false,
+                                    ),
+                                  );
+                            },
                           ),
-                          onChanged: (v) {
-                            password = v;
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    BlocBuilder<LoginBloc, LoginState>(
+                      builder: (context, state) {
+                        return ElevatedButton(
+                          onPressed: () {
                             context.read<LoginBloc>().add(
-                                  TextInputChanged(
-                                    formValid:
-                                        _formKey.currentState?.validate() ??
-                                            false,
-                                  ),
-                                );
+                                LoginClicked(email: email, password: password));
                           },
-                        )
-                      ],
+                          child: Text("Login"),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  BlocBuilder<LoginBloc, LoginState>(
-                    builder: (context, state) {
-                      return ElevatedButton(
-                        onPressed: () {
-                          context.read<LoginBloc>().add(
-                              LoginClicked(email: email, password: password));
-                        },
-                        child: Text("Login"),
-                      );
-                    },
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      appRouter.replace(SignUpScreenRoute());
-                    },
-                    child: Text("Or Signup?"),
-                  ),
-                ],
-              ),
-            ],
+                    TextButton(
+                      onPressed: () {
+                        appRouter.replace(SignUpScreenRoute());
+                      },
+                      child: Text("Or Signup?"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/presentation/sign_up_screen.dart
+++ lib/presentation/sign_up_screen.dart
@@ -16,120 +16,163 @@ class _SignUpScreenState extends State<SignUpScreen> {
   String email = "";
   String password = "";
   String referralCode = "";
+  bool _showPassword = false;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
     return BlocProvider(
       create: (context) => SignupBloc(),
       child: Scaffold(
         body: Form(
           key: _formKey,
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              BlocBuilder<SignupBloc, SignupState>(
-                builder: (context, state) {
-                  return Card(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                            label: Text("Username"),
-                          ),
-                          onChanged: (v) {
-                            email = v;
-                            context.read<SignupBloc>().add(
-                                  InputChanged(
-                                    referralAvaialble: referral,
-                                    formValid:
-                                        _formKey.currentState?.validate() ??
-                                            false,
-                                  ),
-                                );
-                          },
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            label: Text("Password"),
-                          ),
-                          onChanged: (v) {
-                            password = v;
-                            context.read<SignupBloc>().add(
-                                  InputChanged(
-                                    referralAvaialble: referral,
-                                    formValid:
-                                        _formKey.currentState?.validate() ??
-                                            false,
-                                  ),
-                                );
-                          },
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-              CheckboxListTile(
-                value: referral,
-                onChanged: (v) {
-                  setState(() {
-                    referral = !referral;
-                  });
-                },
-                title: Text("have a referral code?"),
-              ),
-              if (referral)
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 BlocBuilder<SignupBloc, SignupState>(
                   builder: (context, state) {
-                    return TextField(
-                      decoration: InputDecoration(
-                        label: Text("Referral code"),
-                      ),
-                      onChanged: (v) {
-                        referralCode = v;
-                        context.read<SignupBloc>().add(
-                              InputChanged(
-                                referralAvaialble: referral,
-                                formValid:
-                                    _formKey.currentState?.validate() ?? false,
+                    return Card(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            validator: (v) {
+                              final regex =
+                                  RegExp(r'^\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b');
+                              if (v != null) {
+                                if (regex.hasMatch(v) != true) {
+                                  return "Please enter a valid email";
+                                }
+                              }
+                            },
+                            decoration: InputDecoration(
+                              label: Text("Email ID"),
+                            ),
+                            onChanged: (v) {
+                              email = v;
+                              context.read<SignupBloc>().add(
+                                    InputChanged(
+                                      referralAvaialble: referral,
+                                      formValid:
+                                          _formKey.currentState?.validate() ??
+                                              false,
+                                    ),
+                                  );
+                            },
+                          ),
+                          TextFormField(
+                            obscureText: !_showPassword,
+                            enableSuggestions: false,
+                            autocorrect: false,
+                            decoration: InputDecoration(
+                              label: Text("Password"),
+                              suffixIcon: InkWell(
+                                child: Icon(Icons.visibility),
+                                onTap: () {
+                                  setState(() {
+                                    _showPassword = !_showPassword;
+                                  });
+                                },
                               ),
-                            );
-                      },
+                              suffixIconColor:
+                                  (!_showPassword) ? Colors.grey : Colors.blue,
+                            ),
+                            validator: (v) {
+                              if (v != null) {
+                                if (v.trim().length < 6) {
+                                  return "Password should be at least 6 digits long";
+                                }
+                              }
+                            },
+                            onChanged: (v) {
+                              password = v;
+                              context.read<SignupBloc>().add(
+                                    InputChanged(
+                                      referralAvaialble: referral,
+                                      formValid:
+                                          _formKey.currentState?.validate() ??
+                                              false,
+                                    ),
+                                  );
+                            },
+                          )
+                        ],
+                      ),
                     );
                   },
                 ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
+                CheckboxListTile(
+                  value: referral,
+                  onChanged: (v) {
+                    setState(() {
+                      referral = !referral;
+                    });
+                  },
+                  title: Text("have a referral code?"),
+                ),
+                if (referral)
                   BlocBuilder<SignupBloc, SignupState>(
                     builder: (context, state) {
-                      return ElevatedButton(
-                        onPressed: () {
-                          if (referralCode.isEmpty) {
-                            context.read<SignupBloc>().add(SignUpClicked(
-                                email: email, password: password));
-                          } else {
-                            context.read<SignupBloc>().add(SignUpClicked(
-                                email: email,
-                                password: password,
-                                referralCode: referralCode));
+                      return TextFormField(
+                        maxLength: 7,
+                        decoration: InputDecoration(
+                          label: Text("Referral code"),
+                        ),
+                        validator: (v) {
+                          final regex = RegExp(r'^(REF)[\d]{4}');
+                          if (v != null) {
+                            if (regex.hasMatch(v) != true) {
+                              return "Incorrect Referral Code";
+                            }
                           }
                         },
-                        child: Text("Sign Up"),
+                        onChanged: (v) {
+                          referralCode = v;
+                          context.read<SignupBloc>().add(
+                                InputChanged(
+                                  referralAvaialble: referral,
+                                  formValid:
+                                      _formKey.currentState?.validate() ??
+                                          false,
+                                ),
+                              );
+                        },
                       );
                     },
                   ),
-                  TextButton(
-                    onPressed: () {
-                      appRouter.replace(LoginScreenRoute());
-                    },
-                    child: Text("Or Login?"),
-                  ),
-                ],
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    BlocBuilder<SignupBloc, SignupState>(
+                      builder: (context, state) {
+                        return ElevatedButton(
+                          onPressed: () {
+                            if (referralCode.isEmpty) {
+                              context.read<SignupBloc>().add(SignUpClicked(
+                                  email: email, password: password));
+                            } else {
+                              context.read<SignupBloc>().add(SignUpClicked(
+                                  email: email,
+                                  password: password,
+                                  referralCode: referralCode));
+                            }
+                          },
+                          child: Text("Sign Up"),
+                        );
+                      },
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        appRouter.replace(LoginScreenRoute());
+                      },
+                      child: Text("Or Login?"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
